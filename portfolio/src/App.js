79745f04f4e19209703d803e344eddd55a7f30e7
Adam Ammour <adamammour@neyzoh.home>
2023-01-04 19:53:58 +0100

    navbar responsive + desktop

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style/index.scss';
+import NavBar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/navbar.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import './navbar.scss'
+
+function NavBar () {
+
+    const [active, setActive] = useState('navbar-menu');
+    const navbarToggle = () => {
+        active === 'navbar-menu' ? setActive('navbar-menu navbar-active') : setActive('navbar-menu')
+    }
+    return (
+        <div className='navbar'>
+            <a href='#' className='navbar-title'>Adam</a>
+                <div className='navbar-links'>
+                    <ul className={active}>
+                        <li className='navbar-item'>
+                            <a href='#' className='navbar-link'>About me</a>
+                        </li>
+                        <li className='navbar-item'>
+                            <a href='#' className='navbar-link'>Skills</a>
+                        </li>
+                        <li className='navbar-item'>
+                            <a href='#' className='navbar-link'>Project</a>
+                        </li>
+                        <li className='navbar-item'>
+                            <a href='#' className='navbar-link'>Contact</a>
+                        </li>
+                    </ul>
+                    <div onClick={navbarToggle} className='navbar-toggler'>
+                        <div className='line1'></div>
+                        <div className='line2'></div>
+                        <div className='line3'></div>
+
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default NavBar;
