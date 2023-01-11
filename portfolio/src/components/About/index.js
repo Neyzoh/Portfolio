640d4465741fc8eece6d200dadd748542ff47405
Adam Ammour <adamammour@neyzoh.home>
2023-01-11 14:18:37 +0100

    skills section finish card + logo language

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -10,7 +10,7 @@ function About() {
             <div className='about-card'>
                 <p className='about-card-paragraph'>Je m&apos;appelle Adam, jeune developer passionné d&apos;informatique.<br></br>
                 <br></br> J&apos;ai acquis de solides connaissances en programmation grâce à mes études et à mes projets personnels,
-                 notamment en HTML&lsquo CSS&lsquo JavaScript&lsquo SQL et React. Je suis passionné par le développement de logiciels et j&apos;aimerais intégrer une entreprise où je pourrais mettre mes compétences en pratique et continuer à apprendre et à me développer en tant que développeur.
+                 notamment en HTML, CSS , JavaScript, SQL et React. Je suis passionné par le développement de logiciels et j&apos;aimerais intégrer une entreprise où je pourrais mettre mes compétences en pratique et continuer à apprendre et à me développer en tant que développeur.
                 </p>
                 <div className='card-btn-container'>
                     <a href={CV} download='CV de Adam'>
--- portfolio/src/components/Skills/index.js
+++ portfolio/src/components/Skills/index.js
@@ -3,8 +3,17 @@ import './style.scss'
 
 function Skills () {
     return(
-        <div className="">
+        <div className="skills-container">
+            <p className='my-skills'>My Skills</p>
 
+            <div className='skill-card'>
+                <div className='card-1'></div>
+                <div className='card-2'></div>
+                <div className='card-3'></div>
+                <div className='card-4'></div>
+                <div className='card-5'></div>
+            </div>
+            
         </div>
     )
 }
